cf685557bdb881e385af6e3372a72af1f5618dff
Ayeromara <isaiahayeromara@gmail.com>
2025-07-01 17:05:39 +0100

    sixth

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Message = require('./models/Message');
+const Message = require('../models/message');
 const Poll = require('../models/Poll');
 
 // GET messages for a room
